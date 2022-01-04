506ee498f8993cdc6f41c96c4edc0bd4cd903331
yangdejun <martonyang0707@163.com>
2022-01-04 17:21:07 +0800

    批量发送短信

--- common-sms/src/main/java/com/awonsome/common/sms/service/SmsService.java
+++ common-sms/src/main/java/com/awonsome/common/sms/service/SmsService.java
@@ -1,13 +1,15 @@
 package com.awonsome.common.sms.service;
 
 import com.aliyun.dysmsapi20170525.Client;
+import com.aliyun.dysmsapi20170525.models.SendBatchSmsRequest;
+import com.aliyun.dysmsapi20170525.models.SendBatchSmsResponse;
 import com.aliyun.dysmsapi20170525.models.SendSmsRequest;
 import com.aliyun.dysmsapi20170525.models.SendSmsResponse;
 import org.springframework.stereotype.Service;
 
 /**
+ * 短信服务
  * @author yangdejun
- * @TODO
  * @date 2022/1/3 22:27
  */
 @Service
@@ -19,7 +21,24 @@ public class SmsService {
         this.smsClient = smsClient;
     }
 
+    /**
+     * 发送短信
+     * @param smsRequest
+     * @return
+     * @throws Exception
+     */
     public SendSmsResponse sendSms(SendSmsRequest smsRequest) throws Exception {
         return smsClient.sendSms(smsRequest);
     }
+
+    /**
+     * 批量发送短信
+     * @param batchSmsRequest
+     * @return
+     * @throws Exception
+     */
+    public SendBatchSmsResponse sendBatchSms(SendBatchSmsRequest batchSmsRequest) throws Exception {
+        return smsClient.sendBatchSms(batchSmsRequest);
+    }
+
 }
